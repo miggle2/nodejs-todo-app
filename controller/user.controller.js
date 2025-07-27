da15faad61e0f9a97163ae13e1cab0bac1438e7f
Mikyung Cho <jmk2633@gmail.com>
2025-07-27 17:05:24 +0900

    회원가입, 로그인

---
+++ controller/user.controller.js
@@ -0,0 +1,47 @@
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+
+const userController = {};
+
+userController.createUser = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+    const user = await User.findOne({ email });
+
+    console.log(user);
+
+    if (user) {
+      throw new Error("이미 가입이 된 유저 입니다.");
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    console.log("hash", hash);
+
+    const newUser = new User({ email, name, password: hash });
+    await newUser.save();
+    res.status(200).json({ status: "success" });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: Error+error });
+  }
+};
+
+userController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }, "-createdAt -updatedAt -__v");
+    if (user) {
+      const isMath = bcrypt.compareSync(password, user.password);
+      if (isMath) {
+        const token = user.generateToken();
+        return res.status(200).json({ status: "success", user, token });
+      }
+    }
+    throw new Error("아이디 또는 비밀번호가 일치하지 않습니다");
+  } catch (error) {
+    res.status(400).json({ status: "fail", error:Error+error });
+  }
+};
+
+module.exports = userController;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const taskApi = require("./task.api");
+const userApi = require("./user.api");
 
 router.use("/tasks", taskApi); // /tasks로 요청이 오면 taskApi로 넘겨주는 것
+router.use("/user", userApi);
 
 module.exports = router;
