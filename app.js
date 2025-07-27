27626e10df7951138d3a07e87a331f8c9697cc19
Mikyung Cho <jmk2633@gmail.com>
2025-07-27 14:23:25 +0900

    1.0.0-1

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index"); // 우리가 만든 router를 연결 하는 것
+require("dotenv").config();
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", indexRouter); // 프론트엔드에서 호출하는 api다 라고 명시한 것
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
   .connect(mongoURI)
@@ -17,6 +22,6 @@ mongoose
     console.log("DB connection fail", err);
   });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server on 5000");
 });
