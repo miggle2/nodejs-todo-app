026388a2a9eda71881ca1c69e000d52cf187545b
Mikyung Cho <jmk2633@gmail.com>
2025-07-28 12:12:21 +0900

    오타수정, 에러메세지 수정

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -23,7 +23,7 @@ userController.createUser = async (req, res) => {
     await newUser.save();
     res.status(200).json({ status: "success" });
   } catch (error) {
-    res.status(400).json({ status: "fail", error: Error+error });
+    res.status(400).json({ status: "fail", message: error.message });
   }
 };
 
@@ -32,15 +32,15 @@ userController.loginWithEmail = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email }, "-createdAt -updatedAt -__v");
     if (user) {
-      const isMath = bcrypt.compareSync(password, user.password);
-      if (isMath) {
+      const isMatch = bcrypt.compareSync(password, user.password);
+      if (isMatch) {
         const token = user.generateToken();
         return res.status(200).json({ status: "success", user, token });
       }
     }
     throw new Error("아이디 또는 비밀번호가 일치하지 않습니다");
   } catch (error) {
-    res.status(400).json({ status: "fail", error:Error+error });
+    res.status(400).json({ status: "fail", message:error.message });
   }
 };
 
